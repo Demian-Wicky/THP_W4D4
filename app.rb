72bcbaf238a7aa1fca4949f70759dd6352811f10
Demian-Wicky <demian.wicky@gmail.com>
2021-08-26 15:22:10 +0200

    1st commit

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'bundler'
+Bundler.require
+
+$:.unshift File.expand_path("./../lib", __FILE__)
+# require 'app/scraper'
+# require 'views/menu'
+# require 'views/done'
+
+# binding.pry
+# puts "end of file "
